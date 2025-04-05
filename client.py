6233447691262b3a1d9376e928deabc3d8836503
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2025-04-05 22:33:07 +0200

    updated window view

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
+
+window_width = 1280
+window_height = 720
+
+pip_width = 320
+pip_height = 180
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    cv2.imshow('Kamera', frame)
+
+    pip_frame = cv2.resize(frame, (pip_width, pip_height))
+    canvas = np.zeros((window_height, window_width, 3), dtype=np.uint8)
+    canvas[0:pip_height, 0:pip_width] = pip_frame
+
+    cv2.imshow('Vero', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
