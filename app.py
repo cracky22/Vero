f2850fed093dd29fa8da88715d249323e0956a2b
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2025-04-05 22:38:37 +0200

    added app + webinterface

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import base64
+
+app = Flask(__name__)
+latest_frame = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    global latest_frame
+    data_url = request.form['frame']
+    header, encoded = data_url.split(',', 1)
+    latest_frame = base64.b64decode(encoded)
+    return '', 204
+
+@app.route('/frame')
+def frame():
+    global latest_frame
+    if latest_frame:
+        return Response(latest_frame, mimetype='image/jpeg')
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
